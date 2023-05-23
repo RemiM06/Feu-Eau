b13bd02433a8e7a6fa6dacedeff515c3200fe074
Rémi MATHIEU <78588508+RemiM06@users.noreply.github.com>
2023-05-23 16:37:53 +0200

    code java niveau 1

--- src/main/java/com/feueau/service/entity/Bloc.java
+++ src/main/java/com/feueau/service/entity/Bloc.java
@@ -12,5 +12,27 @@ public class Bloc {
         this.name = name;
     }
 
+    public double getX() {
+        return x;
+    }
 
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
