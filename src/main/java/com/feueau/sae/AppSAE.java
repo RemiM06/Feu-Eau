f94ed6567b970494438ed16100b3df5736d3f4f3
Rémi MATHIEU <78588508+RemiM06@users.noreply.github.com>
2023-05-31 22:28:55 +0200

    background image

--- src/main/java/com/feueau/sae/AppSAE.java
+++ src/main/java/com/feueau/sae/AppSAE.java
@@ -3,7 +3,10 @@ package com.feueau.sae;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import java.net.URL;
 
 import java.io.IOException;
 
@@ -17,7 +20,21 @@ public class AppSAE extends Application {
     @Override
     public void start(Stage stageMain) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(AppSAE.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 700, 400);
+        Pane rootPane = new Pane();
+
+        URL urlImage = getClass().getResource("/img/Akainu-vs-Aokiji.png");
+
+        Image backgroundImage = new Image(urlImage.toExternalForm());
+        BackgroundImage background = new BackgroundImage(backgroundImage, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT,
+                                                            BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+
+        rootPane.setBackground(new Background(background));
+
+
+
+        Scene scene = new Scene(rootPane, 700, 400);
+        
+
 
         stageMain.setFullScreen(true);
         stageMain.setTitle("Hello!");
