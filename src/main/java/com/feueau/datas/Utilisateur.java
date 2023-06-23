fc6c5a10696bd2e42018f767b3f17325c9f2c824
Rémi MATHIEU <78588508+RemiM06@users.noreply.github.com>
2023-06-23 22:27:49 +0200

    userNameBDD

--- src/main/java/com/feueau/datas/Utilisateur.java
+++ src/main/java/com/feueau/datas/Utilisateur.java
@@ -9,7 +9,7 @@ public class Utilisateur {
 
     public static void AjoutUtilisateur(String nomUtilisateur, String motDePasse, String IP) {
         String url = "jdbc:mysql://134.59.143.50:3306/sae_feueau";
-        String utilisateurBDD = "mr012420";
+        String utilisateurBDD = "remim";
         String motDePasseBDD = "Rqznu7ey";
 
         Connection connexion = null;
