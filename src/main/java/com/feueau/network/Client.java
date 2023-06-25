92f6fd73fb993706070ffc63824e71785b3863cc
Reezord <romain.stefani04@gmail.com>
2023-06-25 16:26:00 +0200

    Commit and push romain

--- src/main/java/com/feueau/network/Client.java
+++ src/main/java/com/feueau/network/Client.java
@@ -5,24 +5,23 @@ import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIONamespace;
 import com.corundumstudio.socketio.SocketIOServer;
 import com.corundumstudio.socketio.listener.ConnectListener;
+import io.socket.emitter.Emitter;
+import io.socket.engineio.client.Socket;
+
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class Client {
-    public static void main(String[] args) {
-        Configuration config = new Configuration();
-        config.setHostname("25.73.214.239");
-        config.setPort(1234);
-
-        final SocketIOServer server = new SocketIOServer(config);
-        SocketIONamespace namespace = server.addNamespace("/");
-
-        namespace.addConnectListener(new ConnectListener() {
+    public static void main(String[] args) throws URISyntaxException {
+        Socket socket = new Socket("ws://25.73.216.239:1234");
+        socket.on(Socket.EVENT_OPEN, new Emitter.Listener() {
             @Override
-            public void onConnect(SocketIOClient client) {
-                System.out.println("Connected to the server");
-                client.sendEvent("message", "Client connected successfully");
+            public void call(Object... args) {
+                socket.send("hi");
+                socket.close();
             }
         });
-
-        server.start();
+        socket.open();
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.feueau.sae {
     requires javafx.fxml;
     requires java.sql;
     requires netty.socketio;
+    requires engine.io.client;
 
 
     opens com.feueau.sae to javafx.fxml;
