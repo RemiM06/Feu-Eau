74a04b1527d2bb393a9eeeb25bc9ca4c4d619530
Rémi MATHIEU <78588508+RemiM06@users.noreply.github.com>
2023-06-25 22:18:08 +0200

    module info java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,7 @@ module com.feueau.sae {
     requires com.fasterxml.jackson.databind;
     requires socket.io.client;
     requires engine.io.client;
+    requires com.fasterxml.jackson.databind;
 
 
     opens com.feueau.sae to javafx.fxml;
